fcdde0ae9b11bf58784f16e4bc02baaed10b6bac
Piyush Ranjan <piyush.ranjan@planetspark.in>
2024-04-30 14:20:08 +0530

    1st test: testing functionality on simple input

--- solution.rb
+++ solution.rb
@@ -1,19 +1,29 @@
-def add(numbers)
-    return sum
-  end
-  
-  def main()
-    while true
-      input = gets.chomp
-  
-      begin
-        result = add(input)
-        puts "Result: #{result}"
-      rescue => exception
-        puts "Error: #{exception}"
-      end
+    def add(numbers)
+        nums = numbers.split(',')
+        nums.map!(&:to_i)
+        sum = nums.sum
+        return sum
     end
-  end
+    
+    def main()
+        while true
+        input = gets.chomp
+    
+        begin
+            result = add(input)
+            puts "Result: #{result}"
+        rescue => exception
+            puts "Error: #{exception}"
+        end
+        end
+    end
+    
+    main()
   
-  main()
-  
+# piyush@piyush-Nitro-AN515-52:~/Desktop/incubyteQues$ ruby solution.rb
+# 1,2,3,4
+# Result: 10
+# 1,2,3,4,10
+# Result: 20
+# 1,2,3,4,50,61,3
+# Result: 124
